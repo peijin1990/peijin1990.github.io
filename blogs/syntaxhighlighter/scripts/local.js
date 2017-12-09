d181863a1d7a80851c3404e57c7190589902eee6
peijin1990 <aparch1995@163.com>
2017-12-09 19:40:29 +0800

    更新blog

---
+++ blogs/syntaxhighlighter/scripts/local.js
@@ -0,0 +1,48 @@
+
+SyntaxHighlighter.defaults['toolbar'] = false; //去掉右上角问号图标
+SyntaxHighlighter.config.tagName = 'pre'; //可以更改解析的默认Tag。
+SyntaxHighlighter.config.bloggerMode = true;
+SyntaxHighlighter.config.stripBrs = true;
+SyntaxHighlighter.all();
+
+$(function() {
+// Line wrap back
+var shLineWrap = function() {
+$('.syntaxhighlighter').each(function() {
+// Fetch
+var $sh = $(this),
+$gutter = $sh.find('td.gutter'),
+$code = $sh.find('td.code');
+// Cycle through lines
+$gutter.children('.line').each(function(i) {
+// Fetch
+var $gutterLine = $(this),
+$codeLine = $code.find('.line:nth-child(' + (i + 1) + ')');
+//alert($gutterLine);
+// Fetch height
+var height = $codeLine.height() || 0;
+if (!height) {
+height = 'auto';
+} else {
+height = height += 'px';
+//alert(height);
+}
+// Copy height over
+$gutterLine.attr('style', 'height: ' + height + ' !important'); // fix by Edi, for JQuery 1.7+ under Firefox 15.0
+console.debug($gutterLine.height(), height, $gutterLine.text(), $codeLine);
+});
+});
+};
+
+
+// Line wrap back when syntax highlighter has done it's stuff
+var shLineWrapWhenReady = function() {
+if ($('.syntaxhighlighter').length === 0) {
+setTimeout(shLineWrapWhenReady, 10);
+} else {
+shLineWrap();
+}
+};
+// Fire
+shLineWrapWhenReady();
+});
